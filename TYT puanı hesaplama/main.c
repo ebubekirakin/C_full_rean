b8c915f6599756fa678ea8deb0692b7aa8cfbbe7
ebubekirakin <ebubekirakin_13@hotmail.com>
2023-10-21 15:23:30 +0300

    TYT puan hesaplama
    TYT ham puan ve yerlestirme puan hesaplama

---
+++ TYT puanı hesaplama/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(int argc, char *argv[]) {
+
+
+float turkce,mat,sos,fen,puan1,puan2,dpl;
+
+
+printf(" *** TYT PUAN HESAPLAMA ARACI *** \n\n");
+
+printf("Turkce Netinizi Girin:");
+scanf("%f",&turkce);
+
+printf("Matematik Netinizi Girin:");
+scanf("%f",&mat);
+
+printf("Sosyal Bilgiler Netinizi Girin:");
+scanf("%f",&sos);
+
+printf("Fen Bilimleri Netinizi Girin:");
+scanf("%f",&fen);
+
+printf("Lise Diploma Notunuz:");
+scanf("%f",&dpl);
+
+puan1=100+turkce*3.2+mat*3.55+sos*3+fen*3.5;
+puan2=puan1+dpl*0.6;
+
+printf("\nHAM PUANINIZ: %f\n",puan1);
+printf("YERLESTIRME PUANINIZ: %f",puan2); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
