c4ed142f808a87eefecb00244bec24106f16bbf5
ebubekirakin <ebubekirakin_13@hotmail.com>
2023-10-26 16:55:27 +0300

    faktoriyel
    faktoriyel hesabi

---
+++ faktöriyel/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+
+int i,n;
+int faktoriyel=1;
+
+printf("Bir Sayi Giriniz:");
+scanf("%d",&n);
+
+for(i=1;i<=n;i++)
+{	
+faktoriyel=faktoriyel*i;
+}
+
+printf("%d",faktoriyel);
+
+
+
+
+	return 0;
+}
