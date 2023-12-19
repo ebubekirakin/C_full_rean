4b942e9c78bc6dde07cfb01530130b620c9dc18e
ebubekirakin <ebubekirakin_13@hotmail.com>
2023-12-19 17:06:41 +0300

    Create main.c

---
+++ 5 e bölünen sayılar/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+
+
+
+printf("****1 ile 50 arasindaki 3'e bolunen sayilar****\n\n");
+
+int i;
+for(i=1;i<=50;i++){
+	
+	if(i%3==0){
+		
+		printf("%d\n",i );
+
+
+    }
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
